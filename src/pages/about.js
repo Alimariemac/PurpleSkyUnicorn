e9e180507f4294e873d916e9f2c6749235223c9e
Alimariemac <Aliciamaccara@gmail.com>
2019-09-07 22:13:04 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,7 +24,7 @@ const AboutPage = () => (
     I am a designer (currently designing @ <strong>Foursquare</strong>) who enjoys playing with code. I spend my free time building silly apps (currently refining a <strong>"divebar roulette" app </strong>), carving out block prints, and reading (current book: <strong>"The Word for World is Forest"</strong> by Ursula K. Le Guin).
     <br></br>
     <br></br>
-    In my past-life I was an English/Kindergarten teacher in Tokyo, a marketer at a company that makes pasta out of a crickets and a C# bootcamp participant.
+    In my past-life I was an English/Kindergarten teacher in Tokyo, a marketer at a company that makes pasta out of crickets and a C# bootcamp participant.
     <br></br>
     <br></br>
     This website was a learning project for me in order to learn Gatsby & Graphql. The morphing shapes were created in P5.js and you can play with them <ExtLink target = "_blank"href="https://editor.p5js.org/Alimariemac/present/3BhtINLch">here</ExtLink>!
